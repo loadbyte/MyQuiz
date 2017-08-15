178ef463ed5fda05f9fa63920e91ef99afde923e
Vinod Kumar M <vino.href@gmail.com>
2017-08-16 00:35:12 +0530

    updated user and pass in config file

--- api/v1/config.php
+++ api/v1/config.php
@@ -2,8 +2,8 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'vino');
-define('DB_PASSWORD', 'indian');
+define('DB_USERNAME', 'myquiz');
+define('DB_PASSWORD', 'myquiz');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'myquiz');
 
